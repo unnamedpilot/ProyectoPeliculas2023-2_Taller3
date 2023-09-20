9499661937dfab041a74672639285eef4fb75d12
UnnamedAlt <83482674+UnnamedAlt@users.noreply.github.com>
2023-09-20 15:06:48 -0500

    Cambios post-workshop3

--- DjangoProjectBase/movie/models.py
+++ DjangoProjectBase/movie/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from openai.embeddings_utils import get_embedding, cosine_similarity
+import numpy as np
 
+
+def get_default_array():
+  default_arr = np.random.rand(1536)  # Adjust this to your desired default array
+  return default_arr.tobytes()
 
 class Movie(models.Model):
   title = models.CharField(max_length=100)
-  description = models.CharField(max_length=1000)
-  image = models.ImageField(upload_to='movie/images/')
+  description = models.CharField(max_length=250)
+  emb = models.BinaryField(default=get_default_array())
+  image = models.ImageField(upload_to='movie/images/', default = 'movie/images/default.jpg')
   url = models.URLField(blank=True)
-  
+
   def __str__(self):
     return self.title
 
